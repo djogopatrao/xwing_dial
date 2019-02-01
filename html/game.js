c2d4da099becc338f3642bfedb1159bee54c5434
Djogo Patrão <djogopatrao@gmail.com>
2019-02-01 19:54:28 -0200

    refactoring event detection

--- html/game.js
+++ html/game.js
@@ -1,7 +1,7 @@
 // the game itself
 var game;
 // the spinning wheel
-var wheel = [];
+var wheel = {};
 // can the wheel spin?
 var canSpin;
 // slices (prizes) placed in the wheel
@@ -37,7 +37,7 @@ playGame.prototype = {
      preload: function(){
           // preloading graphic assets
           game.load.image("wheel", "firespray_dial.png");
-		game.load.image("pin", "pin.png");     
+          game.load.image("pin", "pin.png");     
      },
      // funtion to be executed when the state is created
   	create: function(){
@@ -45,43 +45,39 @@ playGame.prototype = {
   		  game.stage.backgroundColor = "#880044";
 
           for (var i=0;i<dials;i++) {
+              var key = "wheel_"+i;
               // adding the wheel in the middle of the canvas
-      		  wheel[i] = game.add.sprite(game.width /2 , 350*(i+0.5), "wheel");
+      		  wheel[key] = game.add.sprite(game.width /2 , 350*(i+0.5), 'wheel');
               // setting wheel registration point in its center
-              wheel[i].anchor.set(0.5);
+              wheel[key].anchor.set(0.5);
+              wheel[key].inputEnabled = true;
+              wheel[key].events.onInputDown.add(this.rotate,this);
+              wheel[key].__mykey = key;
+
               // adding the pin in the middle of the canvas
               var pin = game.add.sprite(game.width / 2,  350*(i+0.5), "pin");
-              // setting pin registration point in its center
               pin.anchor.set(0.5);
+              pin.inputEnabled = true;
+              pin.__mykey = key;
+              pin.events.onInputDown.add(this.lockDial,this);
           }
 
           // the game has just started = we can spin the wheel
           canSpin = true;
           // waiting for your input, then calling "spin" function
-          game.input.onDown.add(this.rotate, this);		
+          //game.input.onDown.add(this.rotate, this);		
           // TODO rotate swipe
 	},
-     startRotate(e) {
-        mouseIsDown = true;
-        positionStartSwipeX = game.input.x
-    },
-     endRotate(e) {
-        mouseIsDown = false;
-        positionStartSwipeX = game.input.x
-    },
-     doRotate(e) {
-            currentAngle = 45;
-            var spinTween = game.add.tween(wheel).to({
-                angle: currentAngle.toString()
-            }, 500, Phaser.Easing.Quadratic.Out, true );
-    },
-     rotate(e){
+     rotate(o,e){
+        var wheelref = wheel[o.__mykey]
         signal = e.clientX>200?+1:-1;
-        wheelref = Math.floor(game.input.y/350);
         currentAngle = 360/slices * signal; 
-        var spinTween = game.add.tween(wheel[wheelref]).to({
+        var spinTween = game.add.tween(wheelref).to({
             angle: currentAngle.toString()
         }, 500, Phaser.Easing.Quadratic.Out, true );
+    },
+    lockDial(o,e){
+        alert("lock "+o.__mykey)
     }
 }
 
