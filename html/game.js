d4f0ad846b5a647448b0e441335f9d5efb9afd39
Djogo Patrão <djogopatrao@gmail.com>
2019-01-31 22:35:40 -0200

    N dials - Im on fire

--- html/game.js
+++ html/game.js
@@ -1,7 +1,7 @@
 // the game itself
 var game;
 // the spinning wheel
-var wheel, wheel2; 
+var wheel = [];
 // can the wheel spin?
 var canSpin;
 // slices (prizes) placed in the wheel
@@ -9,12 +9,17 @@ var slices = 17;
 // text field where to show the prize
 var prizeText;
 
+var dials;
+
 //
 var currentAngle = 0;
 
 window.onload = function() {	
+
+    dials = prompt("Quantos dials?")*1;
+
      // creation of a 458x488 game
-	game = new Phaser.Game(458, 700, Phaser.AUTO, "");
+	game = new Phaser.Game(458, 350*dials, Phaser.AUTO, "");
      // adding "PlayGame" state
      game.state.add("PlayGame",playGame);
      // launching "PlayGame" state
@@ -38,28 +43,18 @@ playGame.prototype = {
   	create: function(){
           // giving some color to background
   		  game.stage.backgroundColor = "#880044";
-          // adding the wheel in the middle of the canvas
-  		  wheel = game.add.sprite(game.width /2 , game.height * 1 / 4, "wheel");
-          // setting wheel registration point in its center
-          wheel.anchor.set(0.5);
-          // adding the wheel in the middle of the canvas
-  		  wheel2 = game.add.sprite(game.width /2, game.height * 3 / 4, "wheel");
-          // setting wheel registration point in its center
-          wheel2.anchor.set(0.5);
-          // adding the pin in the middle of the canvas
-          var pin = game.add.sprite(game.width / 2, game.height *1/4, "pin");
-          // setting pin registration point in its center
-          pin.anchor.set(0.5);
-          // adding the pin in the middle of the canvas
-          var pin2 = game.add.sprite(game.width / 2, game.height *3/4, "pin");
-          // setting pin registration point in its center
-          pin2.anchor.set(0.5);
-          // adding the text field
-          prizeText = game.add.text(game.world.centerX, 480, "");
-          // setting text field registration point in its center
-          prizeText.anchor.set(0.5);
-          // aligning the text to center
-          prizeText.align = "center";
+
+          for (var i=0;i<dials;i++) {
+              // adding the wheel in the middle of the canvas
+      		  wheel[i] = game.add.sprite(game.width /2 , 350*(i+0.5), "wheel");
+              // setting wheel registration point in its center
+              wheel[i].anchor.set(0.5);
+              // adding the pin in the middle of the canvas
+              var pin = game.add.sprite(game.width / 2,  350*(i+0.5), "pin");
+              // setting pin registration point in its center
+              pin.anchor.set(0.5);
+          }
+
           // the game has just started = we can spin the wheel
           canSpin = true;
           // waiting for your input, then calling "spin" function
@@ -81,11 +76,10 @@ playGame.prototype = {
             }, 500, Phaser.Easing.Quadratic.Out, true );
     },
      rotate(e){
-        console.log(1);
         signal = e.clientX>200?+1:-1;
-        wheelref = e.clientY<488?wheel:wheel2;
+        wheelref = Math.floor(game.input.y/350);
         currentAngle = 360/slices * signal; 
-        var spinTween = game.add.tween(wheelref).to({
+        var spinTween = game.add.tween(wheel[wheelref]).to({
             angle: currentAngle.toString()
         }, 500, Phaser.Easing.Quadratic.Out, true );
     }
