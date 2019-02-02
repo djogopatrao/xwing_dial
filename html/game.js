4ee0c5bddbefb62f17fe3341ea02be4ab2ff2828
Djogo Patrão <djogopatrao@gmail.com>
2019-02-02 01:17:12 -0200

    framework for multiple dials

--- html/game.js
+++ html/game.js
@@ -1,14 +1,20 @@
 // the game itself
 var game;
+
+// 
+var init_dials = [];
+
 // the spinning wheel DIALS
 var wheel = {};
+
 // the back of the DIALS
 var dial_backs = {}
+
 // the pin the DIALS
 var pins = {}
 
-// slices (prizes) placed in the wheel
-var slices = 17;
+// how many maneuvers on eah dial
+var dial_movements = { 'firespray': 17, 'quadjumper': 18 };
 var angles = {}
 var dials;
 
@@ -16,27 +22,47 @@ window.onload = function() {
 
     if ( !window.sessionStorage.getItem('xwingDials') ) {
 
-        dials = prompt("Quantos dials?")*1;
-        for( var i=0;i<dials;i++ ) {
-            angles["wheel_"+i] = 0;
-        }
-
     } else {
 
         var obj = JSON.parse( window.sessionStorage.getItem('xwingDials') )
 
         dials  = obj.dials
-        slices = obj.slices
         angles = obj.angles
+        init_dials = obj.init_dials
+
+        init_playarea()
     }
+
+
+    // hook events to the form
+    $('#add-dial-button').on('click',function(){
+        init_dials.push( $('#select-dial').val() );
+        var txt="";
+        $(init_dials).each(function(i,v){txt+=v+"<br>"});
+        $('#dial-list').html(txt);
+    });
+
+    // hook events to the form
+    $('#show-dial-button').on('click',function(){
+        dials = init_dials.length;
+        for( var i=0;i<dials;i++ ) {
+            angles["wheel_"+i] = 0;
+        }
+        init_playarea();
+    });
+
+}
+
+function init_playarea() {
     // creation of a 458x488 game
-    game = new Phaser.Game(458, 350*dials, Phaser.AUTO, "");
+    game = new Phaser.Game(458, 350*dials, Phaser.AUTO, "main");
     // adding "PlayGame" state
     game.state.add("PlayGame",playGame);
     // launching "PlayGame" state
     game.state.start("PlayGame");
 
-
+    $('#menubar').hide();
+    $('#dial-list').hide();
 }
 
 // PLAYGAME STATE
@@ -46,10 +72,12 @@ var playGame = function(game){};
 playGame.prototype = {
      // function to be executed once the state preloads
      preload: function(){
-          // preloading graphic assets
-          game.load.image("wheel", "firespray_dial.png");
-          game.load.image("back", "dial_back.png");
-          game.load.image("pin", "pin.png");     
+            // preloading graphic assets
+            $(init_dials).each(function(i,v){
+                game.load.image(v+"_dial", v+"_dial.png");
+            });
+            game.load.image("back", "dial_back.png");
+            game.load.image("pin", "pin.png");     
      },
      // funtion to be executed when the state is created
   	create: function(){
@@ -59,11 +87,12 @@ playGame.prototype = {
         for (var i=0;i<dials;i++) {
             var key = "wheel_"+i;
             // adding the wheel in the middle of the canvas
-            wheel[key] = game.add.sprite(game.width /2 , 350*(i+0.5), 'wheel');
+            wheel[key] = game.add.sprite(game.width /2 , 350*(i+0.5), init_dials[i]+"_dial" );
             // setting wheel registration point in its center
             wheel[key].anchor.set(0.5);
             wheel[key].inputEnabled = true;
             wheel[key].__mykey = key;
+            wheel[key].__movements = dial_movements[init_dials[i]];
             game.add.tween(wheel[key]).to({angle:angles[key]},500,Phaser.Easing.Quadratic.Out,true);
             wheel[key].events.onInputDown.add(this.rotate,this);
 
@@ -91,7 +120,7 @@ playGame.prototype = {
     rotate(o,e){
         var wheelref = wheel[o.__mykey]
         var signal = e.clientX>200?+1:-1;
-        var angleIncrement = 360/slices * signal;
+        var angleIncrement = 360/o.__movements * signal;
         var spinTween = game.add.tween(wheelref).to({
             angle: angleIncrement.toString()
         }, 500, Phaser.Easing.Quadratic.Out, true );
@@ -114,8 +143,8 @@ playGame.prototype = {
     saveState(){
         var obj = {
             dials: dials,
-            slices: slices,
-            angles: angles
+            angles: angles,
+            init_dials: init_dials
         };
         window.sessionStorage.setItem('xwingDials', JSON.stringify(obj) )
     }
